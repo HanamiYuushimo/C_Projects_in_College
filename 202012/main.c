cbd1900e98628e3077d8c0151d68314bd6e3dce9
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-07 19:07:32 +0800

    Update main.c

--- 202012/main.c
+++ 202012/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello,World!\n");
-    return 0;
+	int i, j;
+	for (i = 1; i <= 4; i++)
+	{
+		for (j = 1; j <= 4 - i; j++)
+		{
+			printf(" ");
+		}
+		for (j = 1; j <= 2 * i - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (i = 3; i <= 3 && i >= 0; i--)
+	{
+		for (j = 1; j <= 4 - i; j++)
+		{
+			printf(" ");
+		}
+		for (j = 1; j <= 2 * i - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
 }
