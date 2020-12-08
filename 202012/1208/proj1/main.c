25c640ddb2b2558a0136667ac6b07f1233cf1dd8
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-08 08:48:31 +0800

    Add files via upload

---
+++ 202012/1208/proj1/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int a,b,c,d;
+	int max(int a,int b);
+	int min(int a,int b);
+	printf("请输入两个数字，用逗号隔开：");
+	scanf("%d,%d",&a,&b);
+	c=max(a,b);
+	d=min(a,b);
+	printf("最大值为：%d;最小值为：%d。\n",c,d);
+	return 0;
+}
+
+int max(int a,int b)
+{
+	int c;
+	if(a>b) c=a;
+	else c=b;
+	return c;
+}
+
+int min(int a,int b)
+{
+	int c;
+	if(a<b) c=a;
+	else c=b;
+	return c;
+}
