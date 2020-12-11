27cd42993bc0b456ddf6baff2488c30153bf6b49
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-11 21:45:32 +0800

    Update 3.3.5.2 大写字母转小写（变式）.c

--- C语言题目/第三章/3.3.5.2 大写字母转小写（变式）.c
+++ C语言题目/第三章/3.3.5.2 大写字母转小写（变式）.c
@@ -4,7 +4,7 @@ int main()
     char c;
     printf("请输入一个小写字母：");
     scanf("%c",c);
-    if(c>=97&&c<=122) c=c-32;
+    if(c>=97&&c<=122) c=c-32;      //97和122为ASCII码，可用'a'和'z'代替（包含单引号）
     printf("大写为：%c\n",c);
     return 0;
 }
