5f490065187756c3633f5fe243d6ca8a34fe35c6
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-11 12:45:29 +0800

    Update 1.4.1 求两个整数之和.c

--- C语言题目/1.4.1 求两个整数之和.c
+++ C语言题目/1.4.1 求两个整数之和.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,sum;
-    a=123;
-    b=456;
-  	sum=a+b;
-  	printf("123+456=%d.\n");
-  	return 0;
+	int a,b,sum;
+	a=123;
+	b=456;
+	sum=a+b;
+	printf("123+456=%d.\n");
+	return 0;
 }
