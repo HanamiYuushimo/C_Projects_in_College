5869054c04c2a51d678b6e2c9c6ce5d37a17c657
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-11 16:55:12 +0800

    Update 1.4.1.4 求两个整数中的较大数.c

--- C语言题目/第一章/1.4.1.4 求两个整数中的较大数.c
+++ C语言题目/第一章/1.4.1.4 求两个整数中的较大数.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int max(int x,int y);
     int a,b,c;
-    printf("请输入两个整数，用空格隔开：");
+    printf("请输入两个整数，用空格隔开\n");
     scanf("%d%d",&a,&b);
     c=max(a,b);
-    printf("最大值为%d。\n",c);
+    printf("%d和%d中的较大数为：%d。\n",a,b,c);
     return 0;
 }
 
