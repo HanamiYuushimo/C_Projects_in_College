384b01316a4d9279b33112cdd36b1bb0fc979ec4
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-12 09:04:52 +0800

    Update 习题P81 第3题.c

--- C语言题目/第三章/习题P81 第3题.c
+++ C语言题目/第三章/习题P81 第3题.c
@@ -1,1 +1,14 @@
-(待补充)
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int d, p;
+	double r, m, n;
+	d = 300000;
+	p = 6000;
+	r = 0.01;
+	n = p - d * r;
+	m = log(p / n) / log(1 + r);
+	printf("需要%.1f个月能还清。", m);
+	return 0;
+}
