6d8172ff6caf3fcf9a50a977d86a0de83773ffb8
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-29 12:42:31 +0800

    Update 2 输出矩阵.c

--- C语言题目/第五章/2 输出矩阵.c
+++ C语言题目/第五章/2 输出矩阵.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+//课本P125 例5.6
 int main()
 {
-  
-  return 0;
+    int i, j, n = 0;
+    for (i = 1; i <= 4; i++)
+        for (j = 1; j <= 5; j++,n++)
+        {
+            if (n % 5 == 0) printf("\n");
+            printf("%d\t", i * j);
+        }
+    printf("\n");
+    return 0;
 }
