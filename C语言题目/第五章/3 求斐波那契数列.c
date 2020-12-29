67e6a8b3bb74f509be1affff7eff3c751a3a4540
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-29 15:45:46 +0800

    Create 3 求斐波那契数列.c

---
+++ C语言题目/第五章/3 求斐波那契数列.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//课本P129 例5.8
+int main()
+{
+    int f1=1,f2=1,f3;
+    int i;
+    for (i = 1; i <= 5; i++)
+        for (j = 1; j <= 4; j++)
+        {
+            n++;
+            if (n % 4 == 0) printf("\n");
+            printf("%d\t", i * j);
+        }
+    printf("\n");
+    return 0;
+}
