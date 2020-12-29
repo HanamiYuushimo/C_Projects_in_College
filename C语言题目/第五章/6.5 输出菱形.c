9e348592cc4a6da633d967a717421b8bb4aa70e3
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-30 00:19:16 +0800

    Create 6.5 输出菱形.c

---
+++ C语言题目/第五章/6.5 输出菱形.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+//同习题P138 第16题
+int main()
+{
+    int i,j;
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=4-i;j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=3;i<=3&&i>=0;i--)
+    {
+        for(j=1;j<=4-i;j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
