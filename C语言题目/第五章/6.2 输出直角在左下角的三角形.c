38ad406c54aabc8de76acb8094d29997b17240c6
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-29 23:00:07 +0800

    Create 6.2 输出直角在左下角的三角形.c

---
+++ C语言题目/第五章/6.2 输出直角在左下角的三角形.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//输出直角在左下角的三角形
+int main()
+{
+    for(i=1;i<=10;i++)
+    {
+        for(j=1;j<=1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
