faef6e8bfb745327d75e93bf4b08cccc84bb67d5
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-29 15:53:29 +0800

    Update 3 求斐波那契数列.c

--- C语言题目/第五章/3 求斐波那契数列.c
+++ C语言题目/第五章/3 求斐波那契数列.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-//课本P129 例5.8
+//课本P129 例5.8,输出斐波那契数列的前40个数
 int main()
 {
     int f1=1,f2=1,f3;
     int i;
-    for (i = 1; i <= 5; i++)
+    printf("%12d\n%12d\n",f1,f2);
+    for (i = 1; i <= 38; i++)
         for (j = 1; j <= 4; j++)
         {
-            n++;
-            if (n % 4 == 0) printf("\n");
-            printf("%d\t", i * j);
+            f3=f1+f2;
+            printf("%12d\n",f3);
+            f1=f2;
+            f2=f3;
         }
-    printf("\n");
     return 0;
 }
