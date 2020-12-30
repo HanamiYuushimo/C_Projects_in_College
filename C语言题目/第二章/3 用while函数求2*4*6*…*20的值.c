34599507ac4bf9c622cc104ec4575cbe94d8f42b
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-30 09:36:21 +0800

    Update 3 用while函数求2*4*6*…*20的值.c

--- C语言题目/第二章/3 用while函数求2*4*6*…*20的值.c
+++ C语言题目/第二章/3 用while函数求2*4*6*…*20的值.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int p=2,i=4;
-    while(i<=18)
+    double p=2,i=4;
+    while(i<=20)
     {
         p=p*i;
         i=i+2;
     }
-    printf("2*4*6*...*20=%d\n",p);
+    printf("2*4*6*...*20=%f\n",p);
     return 0;
 }
