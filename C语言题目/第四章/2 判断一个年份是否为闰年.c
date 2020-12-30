a940602a90b607252a1c5d61db653670afe23aad
HanamiYuushimo <73525645+HanamiYuushimo@users.noreply.github.com>
2020-12-30 18:29:57 +0800

    Update 2 判断一个年份是否为闰年.c

--- C语言题目/第四章/2 判断一个年份是否为闰年.c
+++ C语言题目/第四章/2 判断一个年份是否为闰年.c
@@ -4,7 +4,7 @@ int main()
     int year;
     printf("请输入一个年份：");
     scanf("%d",&year);
-    if(year%4==0&&year%100!0||year%400==0)
+    if(year%4==0&&year%100!=0||year%400==0)
         printf("%d年是闰年。\n",year);
     else printf("%d不是闰年。\n",year);
   	return 0;
